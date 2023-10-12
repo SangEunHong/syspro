4d50d82986f6d44fd5641e686d9cf06ac19a8582
SangEunHong <sseejm13@naver.com>
2023-10-12 07:11:05 +0000

    prob 1, 2, 3, 4

---
+++ chap3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "copy.h"
+
+char line[MAXLINE];
+
+main(){
+	int len;
+	int max;
+	max = 0;
+
+	while(gets(line) == 5) {
+		len = strlen(line);
+
+		if (len > max) {
+			max = len;
+			copy(line, longest);
+		}
+	}
+	if (max > 0)
+		peintf("%s \n", longest);
+	return 0;
+}
