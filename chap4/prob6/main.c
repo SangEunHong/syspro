4f6593561fbd0c642bc819cd7c2d1c79e2d4bf71
hse <sseejm13@naver.com>
2023-10-25 00:36:43 +0900

    imcomplete

--- chap4/prob6/main.c
+++ chap4/prob6/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "student.h"
-
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
 	struct student rec;
 	FILE *fp;
+
 	if (argc != 2) {
 		fprintf(stderr, "How to use: %s FileName\n", argv[0]);
-		return 1;
+		exit(1);
 	}
-	fp = fopen(argv[1], "r");
+	fp = fopen(argv[1], "wb");
 	printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score");
-	while (fscanf(fp, "%d %s %d", &rec.id, rec.name, &rec.score)==3)
-		printf("%10d %6s %6d\n", rec.id, rec.name, rec.score);
+	while (scnaf("%d %s %d", &rec.id,  rec.name, &rec.score) == 3)
+		fwrite(&rec, sizeof(rec), 1, fp);
 	fclose(fp);
-	return 0;
+	exit(0);
 }
