42ceafee715668aa2f5a1963c6f6e61467780502
hse <sseejm13@naver.com>
2023-11-01 23:48:04 +0900

    complete

---
+++ chap6/prob1/main.c
@@ -0,0 +1,15 @@
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+	int i;
+	struct stat buf;
+	for (i = 1; i < argc; i++) {
+		printf("%s: ", argv[i]);
+	if (lstat(argv[i], &buf) < 0) {
+		perror("lstat()");
+	continue;
